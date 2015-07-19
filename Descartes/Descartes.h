a050b16a5bd435da0749df6ca3eacf11e19c6685
FredrikSjoberg <fredriksjoberg@Fredriks-iMac.lan>
2015-07-19 21:52:15 +0200

    Initial Commit

---
+++ Descartes/Descartes.h
@@ -0,0 +1,19 @@
+//
+//  Descartes.h
+//  Descartes
+//
+//  Created by Fredrik Sjöberg on 19/07/15.
+//  Copyright (c) 2015 Fredrik Sjoberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Descartes.
+FOUNDATION_EXPORT double DescartesVersionNumber;
+
+//! Project version string for Descartes.
+FOUNDATION_EXPORT const unsigned char DescartesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Descartes/PublicHeader.h>
+
+

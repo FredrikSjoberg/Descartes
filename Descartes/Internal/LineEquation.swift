932fc0a6be136b9f405d809a71a636f5b64f0017
Fredrik Sjöberg <fredrik.sjoberg@knightsfee.com>
2015-09-06 14:03:54 +0200

    import message

--- Descartes/Internal/LineEquation.swift
+++ Descartes/Internal/LineEquation.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import CoreGraphics
+
 internal struct LineEquation {
     
     // format: ax + by = c
--- Descartes/Internal/TransformedVertex.swift
+++ Descartes/Internal/TransformedVertex.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreGraphics
 
 internal struct TransformedVertex {
     internal let actualPoint: CGPoint
